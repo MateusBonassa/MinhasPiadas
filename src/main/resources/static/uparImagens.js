38f3771a2bc999c3ac6fb2e07de22f6ad7069561
MateusBonassa <mateusbonassa100@gmail.com>
2024-01-14 20:07:55 -0300

    update 14/1

--- src/main/resources/static/uparImagens.js
+++ src/main/resources/static/uparImagens.js
@@ -22,35 +22,48 @@ const firebaseConfig = {
      
 
   const uploadImage = ()=>{
-    var file = document.getElementById("imagem").files[0];
-    const dataAtual = new Date();
-    const file_name = `${dataAtual.getSeconds()}_${dataAtual.getMinutes()}_${dataAtual.getHours()}_${dataAtual.getDate()}_${dataAtual.getMonth() + 1}_${dataAtual.getFullYear()}`+file.name;
-    //alert(file_name);
-    const storageRef = storage.ref().child("images");
-    const folderRef = storageRef.child(file_name);
-    const uploadtask = folderRef.put(file);
-    var uploadedFileName;
+    const loaderContainer = document.getElementById("loaderContainer");
+    const content = document.getElementById("modalPiImg");
 
-    uploadtask.on(
-        "state_changed",
-        (snapshot) => {
-           
-            uploadedFileName = snapshot.ref.name;
-        },
-        (error) => {
-          reject(error);
-        },
-        () => {
-          storage
-            .ref("images")
-            .child(uploadedFileName)
-            .getDownloadURL()
-            .then((url) => {
-              //alert(url);
-              fazerRequisicao(url,file_name);
-            });
-        }
-      );
+    if(validarDadosCadPiadaImg())
+    {
+      
+      loaderContainer.style.display = "flex";
+      content.style.pointerEvents = "none"; // Desativa interação com o conteúdo
+      var file = document.getElementById("imagem").files[0];
+      const dataAtual = new Date();
+      const file_name = `${dataAtual.getSeconds()}_${dataAtual.getMinutes()}_${dataAtual.getHours()}_${dataAtual.getDate()}_${dataAtual.getMonth() + 1}_${dataAtual.getFullYear()}`+file.name;
+      //alert(file_name);
+      const storageRef = storage.ref().child("images");
+      const folderRef = storageRef.child(file_name);
+      const uploadtask = folderRef.put(file);
+      var uploadedFileName;
+  
+      uploadtask.on(
+          "state_changed",
+          (snapshot) => {
+             
+              uploadedFileName = snapshot.ref.name;
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            storage
+              .ref("images")
+              .child(uploadedFileName)
+              .getDownloadURL()
+              .then((url) => {
+                //alert(url);
+                fazerRequisicao(url,file_name);
+              });
+          }
+        );
+        loaderContainer.style.display = "none";
+        content.style.pointerEvents = "auto";
+
+    }
+    
  
  
     
@@ -109,9 +122,61 @@ function fazerRequisicao(urlImg,nome) {
             alert("Sucesso");
             window.location.reload();
         }
-           else throw Error("erro") 
+        else{
+          alert("Ocorreu um erro na requisição! Tente Novamente!");
+        }
     })
     .catch(error => {
         console.error('Erro na requisição:', error);
     });
+}
+
+
+function validarDadosCadPiadaImg() {
+
+  var titulo2 = document.getElementById("titulo2");
+  var keywords2 = document.getElementById("keywords2");
+  var imagem = document.getElementById("imagem");
+  var allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.ico', '.svg', '.webp', '.jfif'];
+  var correto=true;
+  if (imagem.files.length === 0) {
+    alert('Por favor, escolha um arquivo.');
+    return false;
+  }
+
+  var fileName = imagem.files[0].name;
+  var fileExtension = '.' + fileName.split('.').pop().toLowerCase();
+
+  if (allowedExtensions.indexOf(fileExtension) === -1) {
+      alert('Formato de arquivo não permitido. Por favor, escolha um formato válido.');
+      return false;
+  }
+  if(titulo2.value.length < 5) 
+  { 
+    titulo2.value = "";
+      titulo2.setAttribute('placeholder','Escreva um título maior!');
+      titulo2.style.border = "solid 1px red";
+      
+      correto = false;
+      titulo2.focus();
+  }
+  else 
+  {
+      titulo2.style.border = "none";
+  }
+  if(keywords2.value.length < 4) 
+  { 
+    keywords2.value = "";
+      keywords2.setAttribute('placeholder','Digite no mínimo uma palavra inteira!');
+      keywords2.style.border = "solid 1px red";
+      //auxPonteiro=1;
+      correto = false;
+      keywords2.focus();
+  }
+  else 
+  {
+      keywords2.style.border = "none";
+  }
+
+  return correto;
 }
